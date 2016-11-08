947d5bde06c8dd2fc8d179df892981a895c259a7
jenboyd <jboyd6290@gmail.com>
2016-11-08 15:11:18 -0500

    Add user owned parties
    Working creation of parties after modifying back end.

--- app/components/party-new/component.js
+++ app/components/party-new/component.js
@@ -1,24 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  newParty: {
-    name: null,
-    date: null,
-    location: null,
-  },
+  newParty: {},
 
   actions: {
-
     save () {
-
       let data = this.get('newParty');
       data.party = this.get('party');
-      console.log(data);
+
+      console.log("Form Data is:", data);
       this.sendAction('save', data);
-      this.set('newParty.name', null);
-      this.set('newParty.date', null);
-      this.set('newParty.location', null);
-      this.set('newParty', {});
+      // this.set('newParty.name', null);
+      // this.set('newParty.date', null);
+      // this.set('newParty.location', null);
+      // this.set('newParty', {});
     },
 
     cancel () {
